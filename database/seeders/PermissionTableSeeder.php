b87e5087c603747883621abd57120d8cecbd30b1
HasanAhmad97 <hasahm69z@gmail.com>
2021-10-22 07:55:19 +0000

    first commit

---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+class PermissionTableSeeder extends Seeder
+{
+/**
+* Run the database seeds.
+*
+* @return void
+*/
+public function run()
+{
+
+
+$permissions = [
+
+
+    'create user',
+    'add role',
+    'add permission',
+    'create post',
+    'show users',
+    'add category',
+    'manage post',
+
+
+
+
+
+];
+
+
+
+foreach ($permissions as $permission) {
+
+Permission::create(['name' => $permission]);
+}
+
+
+}
+}
---
+++ database/seeders/createAdmin.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class createAdmin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'hasan',
+            'email' => 'hasan@gmail.com',
+            'password' => bcrypt('12345678'),
+            'roles_name' => "superAdmin",
+            'Status' => 'مفعل',
+        ]);
+
+        $role = Role::create(['name' => 'superAdmin']);
+
+        $permissions = Permission::pluck('id','id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
